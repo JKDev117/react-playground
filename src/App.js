00d77d9a14a6767cb3186446b99521b657e4950c
Jae Kim <jckim81@yahoo.com>
2020-01-31 00:55:24 -0500

    initital commit after completing module 14.8 at Thinkful

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip';
+import Messages from './Messages';
+
+//make 2 tooltips here and another inside the App directly
+const firstToolTip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+    ipsum
+  </Tooltip>
+);
+
+const secondToolTip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officis
+  </Tooltip>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /* We are "calling" the Split component twice, each time with slightly different arguments for children & className */
+    <main className='App'>
+      <Split className='left' flexBasis='2'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+        {/* make another tooltip directly inside the App */}
+        {/* {firstToolTip} */}
+        <Tooltip message="one more tool tip message">
+          {' '}Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+        Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+        
+      </Split>
+      <Messages name="Messages" unread={0}/>
+      <Messages name="Notifications" unread={10}/>
+    </main>
+  )
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,43 @@
+// make React available
 import React from 'react';
-import { render } from '@testing-library/react';
+
+// make the ReactDOM available, necessary for rendering the component
+import ReactDOM from 'react-dom';
+
+// make the App component available
 import App from './App';
+
+// this is the test case (smoke test)
+it('renders without crashing', () => {
+  // first create a DOM element to render the component into
+  const div = document.createElement('div');
+
+  // render the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(<App />, div);
+
+  // clean up code
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+
+
+
+
+/*
+//make React available
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+//make the App component available
+import App from './App';
+
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+*/
