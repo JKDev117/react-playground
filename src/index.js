9751da79dbf8b66e6a551c0ba1146ee9d90134f2
Jae Kim <jckim81@yahoo.com>
2020-02-26 00:57:56 -0500

    added paltrows-power-toes for 14.14 module

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App'; //commented out for 14.14
+import App from './paltrows-power-toes/App';
 import './index.css';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun.js';
+import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(
-    <div>
-        <App />
-        <div className = "state-drills"> 
-            <HelloWorld />
-            <Bomb />
-            <RouletteGun bulletInChamber={8}/>
+ReactDOM.render (
+        <div>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+            {/* //commented out for 14.14
+            <div className = "state-drills"> 
+                <HelloWorld />
+                <Bomb />
+                <RouletteGun bulletInChamber={8}/>
+            </div>
+            */}
         </div> 
-    </div>, document.getElementById('root'));
+    , document.getElementById('root')
+
+);
+    
+    
+    
+    
 
 
 
