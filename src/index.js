9cae1bcd2f4c45c3e40fb81828f27aad6697de35
Jae Kim <jckim81@yahoo.com>
2020-03-11 19:53:03 -0400

    added playingwithdanger & registrationForm for Mod 14.17

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './playingwithdanger/App';
 //import App from './App'; //commented out for 14.14
-import App from './paltrows-power-toes/App';
+//import App from './paltrows-power-toes/App'; //commented out for 14.17
+
+
+
 import './index.css';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun.js';
 import { BrowserRouter } from 'react-router-dom';
 import AppLang from './lang-context/AppLang';
-
+import RegistrationForm from './registrationForm/registrationForm';
 
 ReactDOM.render (
-        <div>
-            <AppLang />
-            {/* //14.14; commented out for 14.15
+        <>
+            {/* 14.17 */}
+            <App />
+
+            {/* 14.17 
+            <RegistrationForm />    
+            */}
+
+            {/* 14.15 */}
+            {/* <AppLang /> */}
+
+            {/* //14.14
             <BrowserRouter>
                 <App />
             </BrowserRouter>
             */}
+            
             {/* //commented out for 14.14
             <div className = "state-drills"> 
                 <HelloWorld />
@@ -25,7 +39,8 @@ ReactDOM.render (
                 <RouletteGun bulletInChamber={8}/>
             </div>
             */}
-        </div> 
+
+        </> 
     , document.getElementById('root')
 
 );
---
+++ src/playingwithdanger/App.js
@@ -0,0 +1,25 @@
+//14.17 Organizing Your React Code; Error Boundaries
+
+import React, {Component} from 'react';
+import Currency from './Currency';
+import CurrencyError from './CurrencyError';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Here are some currency values.</h1>
+                <CurrencyError>
+                    Germany: <Currency value={21} locale="de-DE" currency="US" />
+                </CurrencyError> 
+                <CurrencyError>   
+                    USA: <Currency value={21} locale="en-US" currency="USD" />
+                </CurrencyError>
+                <h1>The currency values displayed are subject to change without notice.</h1>    
+            </div>
+        );
+    }
+}
+
+export default App;
