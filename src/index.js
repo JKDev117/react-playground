bf4725815876bee66b182c3f0a9b6900969fc32a
Jae Kim <jckim81@yahoo.com>
2020-03-12 19:42:44 -0400

    commit for creating-and-reading-context branch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './playingwithdanger/App'; //14.14
 //import App from './App'; //comment this out for 14.14
-import App from './paltrows-power-toes/App'; //14.14 //comment this out for 14.17
+//import App from './paltrows-power-toes/App'; //14.14 //comment this out for 14.15 & above
 import './index.css';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
@@ -24,13 +24,13 @@ ReactDOM.render (
             */}
 
             {/* 14.15 //Comment this out for 14.17 */}
-            {/* <AppLang /> */}
+            <AppLang />
 
-            {/* //14.14 */}
+            {/* //14.14 
             <BrowserRouter>
                 <App />
             </BrowserRouter>
-            
+            */}
 
             {/* Commented out for 14.14 */}
             {/*            
