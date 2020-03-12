b499830a658052fbb4c11ea0c4b4a9816848aac9
Jae Kim <jckim81@yahoo.com>
2020-03-12 19:39:00 -0400

    commit for react-router branch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './playingwithdanger/App'; //14.14
-import App from './App'; //comment this out for 14.14
-//import App from './paltrows-power-toes/App'; //comment this out for 14.17
+//import App from './App'; //comment this out for 14.14
+import App from './paltrows-power-toes/App'; //14.14 //comment this out for 14.17
 import './index.css';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
@@ -18,7 +18,7 @@ ReactDOM.render (
             {/* 14.17 
             <App /> 
             */}
-            
+
             {/* 14.17
             <RegistrationForm />    
             */}
@@ -26,19 +26,21 @@ ReactDOM.render (
             {/* 14.15 //Comment this out for 14.17 */}
             {/* <AppLang /> */}
 
-            {/* //14.14
+            {/* //14.14 */}
             <BrowserRouter>
                 <App />
             </BrowserRouter>
-            */}
             
+
+            {/* Commented out for 14.14 */}
+            {/*            
             <App />
             <div className = "state-drills"> 
                 <HelloWorld />
                 <Bomb />
                 <RouletteGun bulletInChamber={8}/>
             </div>
-            
+            */}
 
         </> 
     , document.getElementById('root')
